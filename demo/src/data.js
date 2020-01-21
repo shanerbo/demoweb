87a6a0fc04ad0e2a3ab297c703e03b76d364a21e
mikedkx <mikedkx@gmail.com>
2020-01-21 00:06:05 -0800

    first adding

---
+++ demo/src/data.js
@@ -0,0 +1,52 @@
+export const storeProducts = [
+    {
+      id: 1,
+      title: "Macbook Pro -15 ",
+      img: "img/product-1.png",
+      price: 3599,
+      company: "APPLe",
+      info:
+        "macbook.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 2,
+      title: "iPhone x",
+      img: "img/product-2.png",
+      price: 1299,
+      company: "APPLE",
+      info:
+        "iphone.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    {
+      id: 3,
+      title: "Mate-30",
+      img: "img/product-3.png",
+      price: 1299,
+      company: "HUAWEI",
+      info:
+        "Mate30.",
+      inCart: false,
+      count: 0,
+      total: 0
+    },
+    
+  ];
+  
+  export const detailProduct = {
+    id: 1,
+    title: "Macbook Pro -15",
+    img: "img/product-1.png",
+    price: 3599,
+    company: "Apple",
+    info:
+      "macbook.",
+    inCart: false,
+    count: 0,
+    total: 0
+  };
